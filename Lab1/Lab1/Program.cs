2e6e63ad717d6c3060fbdfc4b370de6e9c11b3b8
Ilya <villeman.5@yandex.ru>
2019-03-17 00:28:20 +0500

    реализована большая часть функционала

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -8,16 +8,16 @@ namespace Lab1
 {
 	abstract class Employee
 	{
-		private int ID;
-		private string firstName;
-		private string secondName;
-		private string middleName;
-		protected decimal fee;                                    
-		protected decimal rate;
-		protected decimal bounty;
-		private readonly DateTime birthday; 
+		public int ID { get; private set; }
+		public string firstName { get; private set; }
+		public string secondName { get; private set; }
+		public string middleName { get; private set; }
+		public decimal fee { get; protected set; }           //?
+		public decimal rate { get; set; }
+		public decimal bounty { get; set; }
+		public readonly DateTime birthday;
 
-		protected Employee() 
+		protected Employee()
 		{
 			ID = 0;
 			firstName = null;
@@ -51,7 +51,7 @@ namespace Lab1
 		public abstract void avgFeeCompute();
 	}
 
-	class EmployeeFixedFee:Employee
+	class EmployeeFixedFee : Employee
 	{
 		public EmployeeFixedFee
 			(
@@ -63,17 +63,17 @@ namespace Lab1
 			decimal _bounty,
 			DateTime _birthday
 			) : base(_ID, _firstName, _secondName, _middleName, _rate, _bounty, _birthday)
-		{                            
+		{
 			fee = rate + bounty;
 		}
 
 		public override void avgFeeCompute() { fee = rate + bounty; }
 	}
 
-	class EmployeeHourlyFee:Employee
+	class EmployeeHourlyFee : Employee
 	{
 		public EmployeeHourlyFee
-			( 
+			(
 			int _ID,
 			string _firstName,
 			string _secondName,
@@ -81,7 +81,7 @@ namespace Lab1
 			decimal _rate,
 			decimal _bounty,
 			DateTime _birthday
-			) : base(_ID, _firstName, _secondName, _middleName,_rate,_bounty, _birthday) 
+			) : base(_ID, _firstName, _secondName, _middleName, _rate, _bounty, _birthday)
 		{
 			fee = (decimal)20.8 * 8 * rate + bounty;
 		}
@@ -92,57 +92,71 @@ namespace Lab1
 		}
 	}
 
-	//class FeeComparer : IComparer<Employee>
-	//{
-	//	public int Compare(Employee p, Employee q)
-	//	{
-	//		decimal pFee = p.avgFeeCompute();
-	//		decimal qFee = q.avgFeeCompute();
+	class FeeComparer : IComparer<Employee>
+	{
+		public int Compare(Employee p, Employee q)
+		{
+			decimal pFee = p.fee;
+			decimal qFee = q.fee;
 
-	//		if (pFee != qFee)
-	//			if (pFee > qFee)
-	//				return -1;
-	//			else
-	//				return 1;
-	//		else
-	//		{
-	//			return 0;
-	//		}
-	//	}
-	//}
+			if (pFee != qFee)
+				if (pFee > qFee)
+					return -1;
+				else
+					return 1;
+			else
+			{
+				return p.secondName.CompareTo(q.secondName);
+			}
+		}
+	}
 
 	class Organization
 	{
 		private List<Employee> list;
-		private decimal avgFee;
+		public decimal avgFee { get; private set; }
+
 		public Organization()
 		{
 			list = new List<Employee>();
 		}
 
 		public void add(Employee newEmployee)
-		{	
+		{
 			list.Add(newEmployee);
+			avgFeeCompute();
 		}
 
 		public void add(Employee[] newEmployee)
 		{
-			for (int i = 0; i < newEmployee.Length;i++)
+			for (int i = 0; i < newEmployee.Length; i++)
 			{
 				list.Add(newEmployee[i]);
 			}
+			avgFeeCompute();
+		}
+
+		public void avgFeeCompute()
+		{
+			decimal sum = 0;
+			for (int i = 0; i < list.Count; i++)
+			{
+				sum += list[i].fee;
+			}
+
+			avgFee = sum / list.Count;
+		}
+
+		public void sort()
+		{
+			FeeComparer c = new FeeComparer();
+			list.Sort(c);
 		}
 
 		//void fileInput()
 		//{
 
 		//}
-
-		public void sort()
-		{
-			//FeeComparer c = new FeeComparer();
-			//list.Sort(c);
-		}
 
 		//void fileOutput()
 		//{
@@ -154,16 +168,25 @@ namespace Lab1
 	{
 		static void Main(string[] args)
 		{
-			EmployeeFixedFee e1 = new EmployeeFixedFee(0, null, null, null, 10, 5, DateTime.MinValue);
-			EmployeeFixedFee e2 = new EmployeeFixedFee(1, null, null, null, 30, 15, DateTime.MinValue);
-			EmployeeFixedFee e3 = new EmployeeFixedFee(2, null, null, null, 20, 5, DateTime.MinValue);
+			EmployeeFixedFee e1 = new EmployeeFixedFee(0, null, null, null, 10, 0, DateTime.MinValue);
+			EmployeeFixedFee e2 = new EmployeeFixedFee(1, null, null, null, 30, 0, DateTime.MinValue);
+			EmployeeFixedFee e3 = new EmployeeFixedFee(2, null, "bbbbb", null, 20, 0, DateTime.MinValue);
+			EmployeeFixedFee e4 = new EmployeeFixedFee(2, null, "aaaaa", null, 20, 0, DateTime.MinValue);
 
 			Organization o = new Organization();
 			o.add(e1);
 			o.add(e2);
 			o.add(e3);
+			o.add(e4);
 
+			o.sort();
+			o.avgFeeCompute();
 			Console.ReadKey();
 		}
+
+		//static void menu()
+		//{
+
+		//}
 	}
 }
