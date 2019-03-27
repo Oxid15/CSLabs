01811c8e26d432ea442f40284cd0e63a71e6f2aa
Ilya <villeman.5@yandex.ru>
2019-03-27 19:53:06 +0500

    первые структуры

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,21 +1,47 @@
 ﻿using System.Text.RegularExpressions;
 using System.Net;
-//using System;
+using System;
+//using System.IO;
 //using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
 
 namespace Lab2
 {
 	class Analyzer
 	{
+		WebClient client;
 
+		public Analyzer()
+		{
+			client = new WebClient();
+		}
+
+		public void findLinks(string pageURI)
+		{
+			string page = client.DownloadString(new Uri(pageURI));
+			MatchCollection items = Regex.Matches(page, @"<a href=[""\/\w-\.:]+>");
+			
+		}
+
+		//public void findLinks(string pageURI, string fileName)
+		//{
+		//	string page = client.DownloadString(new Uri(pageURI));
+		//	MatchCollection items = Regex.Matches(page, @"<a href=[""\/\w-\.:]+>");
+
+		//	TextWriter writer = new StreamWriter(fileName);
+		//	foreach (var item in items)
+		//	{
+		//		writer.WriteLine(item);
+		//	}
+		//	writer.Close();
+		//}
 	}
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Analyzer a = new Analyzer();
+			a.findLinks("http://www.susu.ru/");
 		}
 	}
 }
