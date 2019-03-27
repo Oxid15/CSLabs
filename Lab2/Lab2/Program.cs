9773db86547bddd98ada6ae811f452700d238c65
Ilya <villeman.5@yandex.ru>
2019-03-27 17:12:23 +0500

    инициализация 2 лабораторной

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using System.Net;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+namespace Lab2
+{
+	class Analyzer
+	{
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
