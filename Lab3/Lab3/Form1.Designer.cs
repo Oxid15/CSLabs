0549e0bffb277885034f5dbebca480c58e3d7a4f
Ilya <villeman.5@yandex.ru>
2019-04-07 00:29:57 +0500

    инициализация третьей работы (после пары)

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// Button1
+			// 
+			this.Button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Button1.AutoSize = true;
+			this.Button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.Button1.BackColor = System.Drawing.Color.Maroon;
+			this.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.Button1.Location = new System.Drawing.Point(344, 331);
+			this.Button1.Name = "Button1";
+			this.Button1.Size = new System.Drawing.Size(41, 30);
+			this.Button1.TabIndex = 0;
+			this.Button1.Text = "OK";
+			this.Button1.UseVisualStyleBackColor = false;
+			this.Button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(639, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(760, 726);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.Button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.HelpButton = true;
+			this.Name = "Form1";
+			this.Opacity = 0.9D;
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
