90c850ecc9056a328bb1e6505d0636bfdc2f5363
Ilya <villeman.5@yandex.ru>
2019-04-11 00:27:50 +0500

    написан рекурсивный алгоритм, но парсер работает корректно не со всеми ссылками

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -6,49 +6,75 @@ using System.Collections.Generic;
 
 namespace Lab2
 {
-	//class ArrayLinks
-	//{
-	//	Match[] links;
-	//	int cursor;
-
-	//	public ArrayLinks(int arrSize)
-	//	{
-	//		links = new Match[arrSize];
-	//		cursor = 0;
-	//	}
-
-	//	public void addToEnd(string link)
-	//	{
-	//		links.SetValue(link, cursor);
-	//		cursor++;
-	//	}
-
-	//}
-
-
 	class Analyzer
 	{
 		WebClient client;
-		List<Match> visitedLinks;
+		static SortedSet<string> visitedLinks;
+		public string root { private set; get; }
 
-		public Analyzer()
+		public Analyzer(string _root)
 		{
+			root = _root;
 			client = new WebClient();
+			visitedLinks = new SortedSet<string>();
 		}
 
-		public MatchCollection findLinksOnPage(string pageURI)
+		static string htmlLinkToURI(string htmlLink)
+		{
+			//FIXME: does not work well with links without quotes
+			int i = 0;
+			while (htmlLink[i] != '\"')
+				i++;
+			i++;
+			//skip a '/' character 
+			if (htmlLink[i] == '/')
+				i++;
+
+			string URI = "";
+			int j = 0;
+			while (htmlLink[i] != '\"')
+			{
+				URI += htmlLink[i];
+				i++;
+			}
+			return URI;
+		}
+
+		public string[] findLinksOnPage(string pageURI)
 		{
 			string page = client.DownloadString(new Uri(pageURI));
-			return Regex.Matches(page, @"<a href=[""\/\w-\.:]+>");
+			MatchCollection matches = Regex.Matches(page, @"<a href=[""\/\w-\.:]+>");
+
+			string[] links = new string[matches.Count];
+			int size = matches.Count;
+			for(int i = 0; i < size;i++)
+			{
+				links[i] = matches[i].ToString();
+			}
+			return links;
 		}
 
-		public void recSearch(string thisURI, int depth)
+		public void recSearch(string thisURI, int depth = 0)
 		{
 			if (depth == 5)
 				return;
-			else //if
-			{	
-				//visitedLinks.Add(thisURI);
+			else if (!visitedLinks.Contains(thisURI))
+			{
+				visitedLinks.Add(thisURI);
+
+				string[] links = findLinksOnPage(thisURI);
+				///////////////////////////TEST
+				foreach (string item in links)
+					Console.WriteLine(item);
+				Console.ReadKey();
+				Console.Clear();
+				///////////////////////////TEST
+				foreach (string link in links)
+				{
+					if (link == "<a href=/contact/feedback/susu/ru>")
+							Console.WriteLine("ы");
+					recSearch(root + htmlLinkToURI(link), depth + 1);
+				}
 			}
 		}
 	}
@@ -57,12 +83,8 @@ namespace Lab2
 	{
 		static void Main(string[] args)
 		{
-			Analyzer a = new Analyzer();
-
-			MatchCollection c;
-			c = a.findLinksOnPage("http://www.susu.ru/");
-			Match[] arr = new System.Text.RegularExpressions.Match[1024];
-			c.CopyTo(arr, 0);
+			Analyzer a = new Analyzer( "https://www.susu.ru/");
+			a.recSearch(a.root); 
 		}
 	}
 }
